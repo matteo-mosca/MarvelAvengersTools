1ec09aba64a378f4dc31a7637d96938200008909
Matteo Mosca <matteo.mosca@gmail.com>
2020-09-24 01:37:09 +0200

    Initial commit.

---
+++ MarvelAvengersBackup/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MarvelAvengersBackup
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var source = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\Marvel's Avengers";
+            var destinationFolder = @"c:\\MarvelAvengersBackup";
+            var destination = $"{destinationFolder}\\{DateTime.Now:yyyy-MM-dd HH-mm-ss}-MarvelAvengersBackup.zip";
+
+            if (!Directory.Exists(destinationFolder))
+            {
+                Directory.CreateDirectory(destinationFolder);
+            }
+
+            ZipFile.CreateFromDirectory(source, destination);
+        }
+    }
+}
---
+++ MarvelAvengersRestore/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MarvelAvengersRestore
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sourceFolder = @"c:\\MarvelAvengersBackup";
+            var destinationFolder = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\Marvel's Avengers";
+
+            if (!Directory.Exists(sourceFolder))
+            {
+                return;
+            }
+
+            var di = new DirectoryInfo(sourceFolder);
+            var latestBackup = di.GetFiles("*-MarvelAvengersBackup.zip", SearchOption.TopDirectoryOnly).OrderByDescending(c => c.LastWriteTime).FirstOrDefault();
+
+            if (latestBackup != null)
+            {
+                Console.WriteLine($"The latest backup found is {latestBackup.Name}. Are you sure you want to overwrite your local saves with this?");
+                var keyPressed = Console.ReadKey(false);
+                if (keyPressed.Key == ConsoleKey.Y)
+                {
+                    ZipFile.ExtractToDirectory(latestBackup.FullName, destinationFolder, true);
+                }
+                
+            }
+        }
+    }
+}
